725e3798a1e3617f93141c65a5c5853317f2717a
Rodrigo da Silva Camargo <142274978+Didico158@users.noreply.github.com>
2024-05-07 17:10:26 -0300

    atualizacao da api

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from settings import HOST, PORT, DEBUG
+
+# import blueprint criado
+from mod_index.index import bp_index
+from mod_funcionario.funcionario import bp_funcionario
+from mod_cliente.cliente import bp_cliente
+from mod_produto.produto import bp_produto
+from mod_login.login import bp_login
+
+
+# Tratamento de exceções na aplicação
+from mod_erro.erro import bp_erro
+
+app = Flask(__name__)
+# registro das rotas do blueprint
+app.register_blueprint(bp_index)
+app.register_blueprint(bp_funcionario)
+app.register_blueprint(bp_cliente)
+app.register_blueprint(bp_produto)
+app.register_blueprint(bp_login)
+app.register_blueprint(bp_erro)
+
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT, debug=DEBUG)
